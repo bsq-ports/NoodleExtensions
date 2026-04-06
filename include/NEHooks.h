e53ef061777a63387e43551379cf7a8158db6272
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-04-05 22:19:07 -0400

    Sort hooks for consistency

--- include/NEHooks.h
+++ include/NEHooks.h
@@ -3,18 +3,24 @@
 
 class Hooks {
 private:
-  static inline std::vector<void (*)()> installFuncs;
+/// Store function name and install function pointer for all hooks, so we can sort and install them in the correct order
+  static inline std::vector<std::pair<std::string, void (*)()>> installFuncs;
 
   static inline bool NoodleHookEnabled;
 
 public:
-  static void AddInstallFunc(void (*installFunc)()) {
-    installFuncs.push_back(installFunc);
+  static void AddInstallFunc(std::string name, void (*installFunc)()) {
+    installFuncs.push_back(std::make_pair(name, installFunc));
   }
 
   static void InstallHooks() {
+    std::sort(installFuncs.begin(), installFuncs.end(),
+              [](const std::pair<std::string, void (*)()>& a, const std::pair<std::string, void (*)()>& b) {
+                return a.first < b.first;
+              });
+
     for (auto installFunc : installFuncs) {
-      installFunc();
+      installFunc.second();
     }
   }
 
@@ -30,7 +36,7 @@ public:
 #define NEInstallHooks(func)                                                                                           \
   struct __NERegister##func {                                                                                          \
     __NERegister##func() {                                                                                             \
-      Hooks::AddInstallFunc(func);                                                                                     \
+      Hooks::AddInstallFunc(std::string(#func), func);                                                                 \
       NELogger::Logger.info("NEHooks Registered install func: " #func);                                                \
     }                                                                                                                  \
   };                                                                                                                   \
